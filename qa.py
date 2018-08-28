3c6d1f62c58ad0784ab2613f019b6fcadcf90fb5
Ilya Makovoz <imakovoz93@gmail.com>
2018-08-28 17:16:13 -0400

    EoD update

--- qa.py
+++ qa.py
@@ -1,14 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+def GetNetworkResources(driver):
+    Resources = driver.execute_script("return window.performance.getEntries();")
+    for resource in Resources:
+        print(resource['name'])
+    return Resources
+
 caps = DesiredCapabilities.CHROME
 caps['loggingPrefs'] = {'performance': 'ALL'}
 driver = webdriver.Chrome(desired_capabilities=caps)
 
 driver.get('https://calero.com')
 
-for entry in driver.get_log('performance'):
-    print (entry)
-    print ('************')
+GetNetworkResources(driver)
+
+# for entry in driver.get_log('performance'):
+#     print (entry)
+#     print ('************')
 
 driver.close()
