e4afb1d49fba3541d9f2a50a219b2c26780bdfc5
Ilya Makovoz <imakovoz93@gmail.com>
2018-09-07 14:12:09 -0400

    cleanup

--- qa.py
+++ qa.py
@@ -1,22 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
 
 def GetNetworkResources(driver):
     Resources = driver.execute_script("return window.performance.getEntries();")
+    debugger
     for resource in Resources:
         print(resource['name'])
     return Resources
 
 caps = DesiredCapabilities.CHROME
 caps['loggingPrefs'] = {'performance': 'ALL'}
-driver = webdriver.Chrome(desired_capabilities=caps)
+options = webdriver.ChromeOptions()
+options.add_argument("--user-data-dir=/Users/imakovoz/Library/Application Support/Google/Chrome/Profile 2")
+driver = webdriver.Chrome(desired_capabilities=caps, options=options)
 
 driver.get('https://calero.com')
 
 GetNetworkResources(driver)
-
-# for entry in driver.get_log('performance'):
-#     print (entry)
-#     print ('************')
 
 driver.close()
